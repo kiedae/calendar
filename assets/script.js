c87c46ca873b5d3616c9a3abec7f94c5f0d646d2
Kiedae <ernestwilliams335@gmail.com>
2023-08-24 23:24:32 -0400

    added classes to time boxes based on the cureent time

--- assets/script.js
+++ assets/script.js
@@ -35,32 +35,214 @@ $(function callInteract() {
     setTimeout(updateCurrentTime, 1000);
   }
   
-
+  function nineTime() {
+    var currentTime = dayjs();
+    var StartTime = dayjs("8:59 AM", 'h:mm A');
+    var EndTime = dayjs("10:00 AM", 'h:mm A');
   
+    if (currentTime.isAfter(EndTime)) {
+        $('#hour-9').removeClass("present");
+        $('#hour-9').addClass("past");
+    } else if (currentTime < StartTime) {
+        $('#hour-9').addClass("future");
+        $('#hour-9').removeClass("present");
+        $('#hour-9').removeClass("past");
+    } else {
+        $('#hour-9').removeClass("future");
+        $('#hour-9').removeClass("past");
+        $('#hour-9').addClass("present");
+    }
+  
+  }
+
+
+  function tenTime() {
+  var currentTime = dayjs();
+  var tenStartTime = dayjs("9:59 AM", 'h:mm A');
+  var tenEndTime = dayjs("11:00 AM", 'h:mm A');
+
+  if (currentTime.isAfter(tenEndTime)) {
+      $('#hour-10').removeClass("present");
+      $('#hour-10').addClass("past");
+  } else if (currentTime < tenStartTime) {
+      $('#hour-10').addClass("future");
+      $('#hour-10').removeClass("present");
+      $('#hour-10').removeClass("past");
+  } else {
+      $('#hour-10').removeClass("future");
+      $('#hour-10').removeClass("past");
+      $('#hour-10').addClass("present");
+  }
+
+}
+ 
+function elevenTime() {
+    var currentTime = dayjs();
+    var StartTime = dayjs("11:00 AM", 'h:mm A');
+    var EndTime = dayjs("12:00 PM", 'h:mm A');
+  
+    if (currentTime.isAfter(EndTime)) {
+        $('#hour-11').removeClass("present");
+        $('#hour-11').addClass("past");
+    } else if (currentTime < StartTime) {
+        $('#hour-11').addClass("future");
+        $('#hour-11').removeClass("present");
+        $('#hour-11').removeClass("past");
+    } else {
+        $('#hour-11').removeClass("future");
+        $('#hour-11').removeClass("past");
+        $('#hour-11').addClass("present");
+    }
+  
+  }
+
+  function twelveTime() {
+    var currentTime = dayjs();
+    var StartTime = dayjs("12:00 PM", 'h:mm A');
+    var EndTime = dayjs("1:00 PM", 'h:mm A');
+  
+    if (currentTime.isAfter(EndTime)) {
+        $('#hour-12').removeClass("present");
+        $('#hour-12').addClass("past");
+    } else if (currentTime < StartTime) {
+        $('#hour-12').addClass("future");
+        $('#hour-12').removeClass("present");
+        $('#hour-12').removeClass("past");
+    } else {
+        $('#hour-12').removeClass("future");
+        $('#hour-12').removeClass("past");
+        $('#hour-12').addClass("present");
+    }
+  
+  }
+
+  function oneTime() {
+    var currentTime = dayjs();
+    var StartTime = dayjs("1:00 AM", 'h:mm A');
+    var EndTime = dayjs("2:00 pM", 'h:mm A');
+  
+    if (currentTime.isAfter(EndTime)) {
+        $('#hour-1').removeClass("present");
+        $('#hour-1').addClass("past");
+    } else if (currentTime < StartTime) {
+        $('#hour-1').addClass("future");
+        $('#hour-1').removeClass("present");
+        $('#hour-1').removeClass("past");
+    } else {
+        $('#hour-1').removeClass("future");
+        $('#hour-1').removeClass("past");
+        $('#hour-1').addClass("present");
+    }
+  
+  }
+
+  function twoTime() {
+    var currentTime = dayjs();
+    var StartTime = dayjs("2:00 PM", 'h:mm A');
+    var EndTime = dayjs("3:00 PM", 'h:mm A');
+  
+    if (currentTime.isAfter(EndTime)) {
+        $('#hour-2').removeClass("present");
+        $('#hour-2').addClass("past");
+    } else if (currentTime < StartTime) {
+        $('#hour-2').addClass("future");
+        $('#hour-2').removeClass("present");
+        $('#hour-2').removeClass("past");
+    } else {
+        $('#hour-2').removeClass("future");
+        $('#hour-2').removeClass("past");
+        $('#hour-2').addClass("present");
+    }
+  
+  }
+
+  function threeTime() {
+    var currentTime = dayjs();
+    var StartTime = dayjs("3:00 PM", 'h:mm A');
+    var EndTime = dayjs("4:00 PM", 'h:mm A');
+  
+    if (currentTime.isAfter(EndTime)) {
+        $('#hour-3').removeClass("present");
+        $('#hour-3').addClass("past");
+    } else if (currentTime < StartTime) {
+        $('#hour-3').addClass("future");
+        $('#hour-3').removeClass("present");
+        $('#hour-3').removeClass("past");
+    } else {
+        $('#hour-3').removeClass("future");
+        $('#hour-3').removeClass("past");
+        $('#hour-3').addClass("present");
+    }
+  
+  }
+
+  function fourTime() {
+    var currentTime = dayjs();
+    var StartTime = dayjs("4:00 PM", 'h:mm A');
+    var EndTime = dayjs("5:00 PM", 'h:mm A');
+  
+    if (currentTime.isAfter(EndTime)) {
+        $('#hour-4').removeClass("present");
+        $('#hour-4').addClass("past");
+    } else if (currentTime < StartTime) {
+        $('#hour-4').addClass("future");
+        $('#hour-4').removeClass("present");
+        $('#hour-4').removeClass("past");
+    } else {
+        $('#hour-4').removeClass("future");
+        $('#hour-4').removeClass("past");
+        $('#hour-4').addClass("present");
+    }
+  
+  }
+
+  function fiveTime() {
+    var currentTime = dayjs();
+    var StartTime = dayjs('11:00 PM', 'h:mm A');
+    var EndTime = dayjs('11:59 PM', 'h:mm A');
+
+
+    if (currentTime.isAfter(EndTime)) {
+        $('#hour-5').removeClass("present");
+        $('#hour-5').addClass("past");
+        $('#hour-5').removeClass('future');
+    } else if (currentTime < StartTime) {
+        $('#hour-5').addClass("future");
+        $('#hour-5').removeClass("present");
+        $('#hour-5').removeClass("past");
+    } else {
+        $('#hour-5').removeClass("future");
+        $('#hour-5').removeClass("past");
+        $('#hour-5').addClass("present");
+    }
+    console.log("Current Time:", currentTime.format('h:mm A'));
+    console.log("Start Time:", StartTime);
+    console.log("End Time:", EndTime);
+  }
+
+
+ 
+
+ 
+ 
   $(document).ready(function() {
     // Start updating the time
     updateCurrentTime();
 
-  $(function classTimeAdjustment() {
-   var tenTime = dayjs('10:59 AM', 'h:mm A');
-   var currentTime = dayjs();
-  
+    $(document).ready(function() {
+        // Start updating the time
+        updateCurrentTime();
+         // Calls all the hour functions
+         nineTime();
+         tenTime();
+         elevenTime();
+         twelveTime();
+         oneTime();
+         twoTime();
+         threeTime();
+         fourTime();
+         fiveTime();
 
-   if (currentTime.isAfter(tenTime)) {
-    $('#hour-10').removeClass("present");
-    $('#hour-10').addClass("past");
-  } else if (currentTime.isAfter("9:59 AM", 'h:mm A') && currentTime.isBefore("11:00 AM", 'h:mm A'))  {
-    $('#hour-10').addClass("present");
-    $('#hour-10').removeClass("past");
-    $('#hour-10').removeClass("future");
-  } else {
-    $('#hour-10').removeClass("present");
-    $('#hour-10').removeClass("past");
-    $('#hour-10').addClass("future");
-  }
-
-
-    classTimeAdjustment();
-  });
+    });
   });
  
